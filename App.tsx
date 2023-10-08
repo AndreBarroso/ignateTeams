6d9c3b895535572235d037a0f1c98a33e2c8177d
Andre Barroso <andrefb.eng1@gmail.com>
2023-10-08 15:13:44 -0300

    feat: inclusao de fontes personalizadas do google e carregamento delas

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import {ThemeProvider} from 'styled-components'
+import {ActivityIndicator} from 'react-native'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto" // npm install expo-font @expo-google-fonts/roboto
 import { Groups } from '@screens/groups';
 import theme from '@theme/index'
 export default function App() {
+
+  const [ fontsLoaded ] = useFonts({Roboto_400Regular, Roboto_700Bold }) // variavel que vai para true quando as fontes são carregadas
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+     { fontsLoaded  ? <Groups /> : <ActivityIndicator/>}
     </ThemeProvider>
   );
 }
