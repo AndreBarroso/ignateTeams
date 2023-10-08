caa4766f998f8cf50f85169788cb1f05e3e7d925
Andre Barroso <andrefb.eng1@gmail.com>
2023-10-08 13:52:52 -0300

    feat: compartilhar o theme para toda aplicação

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
+import {ThemeProvider} from 'styled-components'
 import { Groups } from '@screens/groups';
+import theme from '@theme/index'
 export default function App() {
   return (
+    <ThemeProvider theme={theme}>
       <Groups />
+    </ThemeProvider>
   );
 }
