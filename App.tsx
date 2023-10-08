d14a6ba0a84d19355154802ee5b5a0b87181a554
Andre Barroso <andrefb.eng1@gmail.com>
2023-10-08 16:53:47 -0300

    feat: estilizando status bar

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
 import {ThemeProvider} from 'styled-components'
-import {ActivityIndicator} from 'react-native'
+
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto" // npm install expo-font @expo-google-fonts/roboto
 import { Groups } from '@screens/groups';
 import theme from '@theme/index'
 import { Loading } from '@components/Loading/indext';
+import { StatusBar } from 'react-native';
+
 export default function App() {
 
   const [ fontsLoaded ] = useFonts({Roboto_400Regular, Roboto_700Bold }) // variavel que vai para true quando as fontes são carregadas
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
      { fontsLoaded  ? <Groups /> : <Loading/>}
     </ThemeProvider>
   );
