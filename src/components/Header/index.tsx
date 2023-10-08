204cfdbc693f8ce696ede02b0521ddb09b3ec239
Andre Barroso <andrefb.eng1@gmail.com>
2023-10-08 19:48:31 -0300

    feat: utilizado bilioteca de icones e styled de um botão

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,20 @@
-import { Container, Logo } from './styles';
+import { BackButton, BackIcon, Container, Logo } from './styles';
+
 import logoImg from '@assets/logo.png'
 
-export  function Header() {
+type Props = {
+  showBackButton?: boolean
+}
+
+export  function Header({showBackButton}: Props) {
   return (
     <Container>
+      {
+        showBackButton && 
+        <BackButton  onPress={() => console.log('lakala')} activeOpacity={0.2} >
+            <BackIcon/>
+        </BackButton>
+        }
         <Logo source={logoImg}/>
     </Container>
   );
