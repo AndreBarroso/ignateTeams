b4e824413131a34040f264f4191f471f26beace1
Andre Barroso <andrefb.eng1@gmail.com>
2023-10-08 10:57:18 -0300

    feat: difinindo mapeamento de rotas de arquivos e suas tipagens

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo'], //adicionar esse preset
+    plugins: [ //adiconar o plugin -> precisou " instalar npm install --save-dev babel-plugin-module-resolver "
+      [
+        'module-resolver', 
+        {
+          root: ['./src'], //diz onde é a pasta raíz da aplicação
+          alias: { //aqui vai o mapeamneto das rotas dos arquivos mapaeamento 
+            '@assets': './src/assets', 
+            '@components': './src/components',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
+          }
+        }
+      ]
+    ]
   };
 };
