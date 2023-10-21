499c642c24af425c8bb097b097482fec53862f09
Andre Barroso <andrefb.eng1@gmail.com>
2023-10-21 11:56:40 -0300

    feat: removendo glit entre trasicao de telas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native' //usando o core de navegação
 import { AppRoutes } from './app.routes'
+import { View } from 'react-native'
+import { useTheme } from 'styled-components'
 
 export function Routes() {
-    return (
-        
-        <NavigationContainer> 
-            <AppRoutes/>
-        </NavigationContainer>
+    const { COLORS } = useTheme() 
 
+    //Adicionando view para não aparecer tela branca entre as trasições de tela
+    return (
+        <View style={{flex: 1, backgroundColor: COLORS.GRAY_600}}>
+            <NavigationContainer> 
+                <AppRoutes/>
+            </NavigationContainer>
+        </View>
     )
 }
