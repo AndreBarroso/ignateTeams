2c1b31df7ed12c35911c90f5ab56d8841463f2a9
Andre Barroso <andrefb.eng1@gmail.com>
2023-10-22 11:01:08 -0300

    feat: cria classe de erros personalizada e trata exceções

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -6,6 +6,8 @@ import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 import { groupCreate } from "@storage/group/groupCreate";
+import { Alert } from "react-native";
+import { AppError } from "@utils/AppErros";
 
 export function NewGroup() {
     const [group, setGroup ] = useState('')
@@ -14,9 +16,19 @@ export function NewGroup() {
     async function handleNew() {
         
         try {
+            if(group.trim().length === 0) {
+                return Alert.alert('Novo Grupo', 'Informe o nome da turma.')
+            }
+
             await groupCreate(group)
             navigation.navigate('players', {group: group}) // passa o parametro para o componente de players
+
         } catch (error){
+            if(error instanceof AppError) {
+                Alert.alert('Novo Grupo', error.message)
+            } else {
+                Alert.alert('Novo Grupe', 'Não foi possível criar um novo grupo.')
+            }
           console.log(error)
         }
         
--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,11 +1,18 @@
 import AsyncStorag from "@react-native-async-storage/async-storage"
 import { GROUP_COLLECTION } from "@storage/storageConfig";
 import { groupsGetAll } from "./groupsGetAll";
+import { AppError } from "@utils/AppErros";
 
 export async function groupCreate(newGroup: string) {
     try {
 
         const storedGroups = await groupsGetAll()
+
+        const groupAlreadyExists = storedGroups.includes(newGroup)
+
+        if(groupAlreadyExists) {
+            throw new AppError('Já existe um grupo cadastrado com esse nome.')
+        }
 
         const storage = JSON.stringify([...storedGroups, newGroup])
         
